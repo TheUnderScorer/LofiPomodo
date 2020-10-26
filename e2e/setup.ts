7c401618a03207a6fae62789470fd07543bdd166
Przemysław Żydek <przemyslaw.zydek@gmail.com>
2020-10-26 08:41:58 +0100

    ci: Add e2e tests

--- e2e/setup.ts
+++ e2e/setup.ts
@@ -1,13 +1,46 @@
 import { resolve } from 'path';
 import { Application } from 'spectron';
 import Electron from 'electron';
+import fetch from 'node-fetch';
 
 export let app: Application;
 
+const waitForRenderer = async () => {
+  return new Promise((resolve) => {
+    const intervalId = setInterval(async () => {
+      try {
+        const response = await fetch('http://localhost:3000');
+
+        if (response.status === 200) {
+          clearInterval(intervalId);
+          resolve();
+        }
+      } catch {
+        // Nothing here ;)
+      }
+    }, 2000);
+  });
+};
+
 beforeEach(async () => {
+  console.log(`Creating app using path ${Electron}`);
+  console.log('Waiting for renderer...');
+
+  await waitForRenderer();
+
+  console.log('Renderer ready!');
+
   app = new Application({
     path: Electron as any,
     args: [resolve(__dirname, '../build/electron.js')],
+    quitTimeout: 20000,
+    waitTimeout: 20000,
+    startTimeout: 10000,
+    chromeDriverArgs: [
+      '--remote-debugging-port=9222',
+      '--no-sandbox',
+      '--disable-dev-shm-usage',
+    ],
   });
 
   await app.start();
--- jest.config.e2e.js
+++ jest.config.e2e.js
@@ -2,5 +2,5 @@ module.exports = {
   preset: 'ts-jest',
   testMatch: ['<rootDir>/e2e/**/*.test.ts?(x)'],
   setupFilesAfterEnv: ['<rootDir>/e2e/setup.ts'],
-  testTimeout: 10000,
+  testTimeout: 900000,
 };
